1e887524167def976010de666974db369cad1a17
RAMK15 <tcs15b023@iittp.ac.in>
2021-10-03 15:59:51 +0000

    Replace components with functions

--- src/App.js
+++ src/App.js
@@ -15,34 +15,6 @@ class App extends Component{
       </div>
     );
   }
-  /*render(){
-    const name = "Ram";
-    const hello = () => "Hello";
-    const loading = false;
-    const showName = true;
-    return (
-      <div className="App">
-        {loading?<h1>Loading...</h1>:<h1>{hello()} {showName && name.toUpperCase()} to {this.getApp()}</h1>}
-        <Navbar title='Github-Finder' icon='fab fa-github'/>
-        <UserItem/>
-      </div>
-    );
-  }*/
-  /*render(){
-    return (
-      <React.Fragment>
-        <h1>Hello Ram in fragment</h1>
-      </React.Fragment>
-    );
-  }*/
-  /*render(){
-    return(
-      <>
-        <h1>Hello Ram in empty angular bracket</h1>
-      </>
-    );
-  }*/
- 
 }
 
 export default App;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,20 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-export class UserItem extends Component {
-    /*constructor(){
-        super();
-        this.state = {
-            id: 'RAM-IMANAGE',
-            login: 'RAM-IMANAGE',
-            avatar_url: 'https://avatars.githubusercontent.com/u/53936481?v=4',
-            html_url : 'https://github.com/RAM-IMANAGE'
-        }
-        console.log(123)
-    }*/
-  render() {
-      /*To avoid using this.state.avatar_url always
-    const {login, avatar_url, html_url} = this.state;*/
-    const {login, avatar_url, html_url} = this.props.user;
+ const UserItem = ({user:{login,avatar_url,html_url}}) => {
     return (
       <div className='card text-center'>
         <img src={avatar_url} alt='' className='round-img' style={{width: '60px'}}/>
@@ -24,7 +11,9 @@ export class UserItem extends Component {
         </div>
       </div>
     );
-  }
 }
 
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
+}
 export default UserItem
