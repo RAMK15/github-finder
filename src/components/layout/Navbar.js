662f706306b886722a0c90a10948f46e92efa331
RAMK15 <tcs15b023@iittp.ac.in>
2021-10-08 15:52:09 +0000

    Router

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import {Link} from 'react-router-dom'
 const Navbar = ({icon, title}) => {
-  
+    /* Use link instead of <a> tag when you want to preserve the state of previous page i.e, search a user go to about and come back, the list of users should be there*/
     return (
       <nav className='navbar bg-primary'>
         <h1><i className={icon}/> {title}</h1>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
       </nav> 
     )
 }
